765cc66907eb92f9967d6e4cbe711f328beb1a75
Andrey <skullikk@mail.ru>
2022-08-05 08:58:15 +0300

    Add descriptions of functions

--- functions.py
+++ functions.py
@@ -2,21 +2,25 @@ import json
 
 
 def load_candidates():
+    """Получение данных из файла"""
     with open('candidates.json', encoding='utf-8') as file_read:
         data = json.load(file_read)
     return data
 
 def get_all():
+    """Функция возвращает все данные по кандидатам"""
     candidates = load_candidates()
     return candidates
 
 def get_by_pk(pk):
+    """Функция возвращает кандидата по его pk"""
     candidates = load_candidates()
     for canditate in candidates:
         if canditate['pk'] == pk:
             return canditate
 
 def get_by_skill(skill_name):
+    """Функция возвращает список кандидатов по скиллу"""
     candidates = load_candidates()
     skill_candidate = []
     for canditate in candidates:
